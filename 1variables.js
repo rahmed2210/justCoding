c1eac131e5dde8d2171dc0d12336d82be1b55981
Rubal <rahmed2210@gmail.com>
2023-09-12 23:08:56 -0400

    1stdayofday

---
+++ 1variables.js
@@ -0,0 +1,45 @@
+// A variable is a named storage for data
+
+// We can use variables to store goodies, visitors, and other data
+
+// --------------------------------------------------------------------------------------
+
+
+//          let  -- keyword
+
+//  use this keyword LET in modern javaScript to declare a variable
+
+//   Variables are Containers for Storing Data
+//   JavaScript Variables can be declared in 4 ways: Automatically. 
+//       Using var. Using let. Using const.
+
+/*
+        let message = "hello"
+    
+        the concept of variable can be easily grasped if we imagine it as a box for data 
+        with a uniquely named sticker on it
+
+        the variable message can be imagined as a box labeled message with a value hello inside
+
+
+        let message
+
+        message = 'hello';
+        message = 'world'; // value changed from hello to world
+        alert(message); // world
+*/
+
+
+            let hello = 'hello word!';
+            let message;
+        //--> copy 'hello world' from hello into message
+
+            message = hello;
+        //--> now two variable hold the same data
+
+     //   alert(hello); // hello world
+    //    alert(message); // hello word
+
+        console.log(hello)
+        console.log(message)
+
---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// <script>
+//         alert("Are you ready to code")
+//     </script>
+
+console.log("Hello i am the first JS file")
