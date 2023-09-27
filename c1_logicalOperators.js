641b253580f3a5139888ce69287041ec09b63691
Rubal <rahmed2210@gmail.com>
2023-09-27 11:22:12 -0400

    day 6

---
+++ c1_logicalOperators.js
@@ -0,0 +1,27 @@
+/*
+            Logical Operators
+
+        OR                        ( || )
+        AND                       ( & )
+        NOT                       ( ! )
+        Nullish Coalescing        ( ?? )
+ */
+
+/*
+        OR Operator
+            true || true  =>  true
+            true || false  =>  true
+            false || true  =>  true
+            false || false  =>  false
+*/
+
+
+a = 12
+b = 23
+c = 56
+
+if(a==12 || b ==23 || c==56) {
+    console.log(true returned)
+} else {
+    console.log{"False Returned"}
+}
---
+++ c2_nullishCoalescing.js
@@ -0,0 +1,15 @@
+
+
+//   Nullish coalescing operator
+
+
+//   ??
+
+//  a??b   => if a is defined then a else b
+
+let user = "Azu"
+    
+alert(user??"anonymouse") 
+    
+    
+   
