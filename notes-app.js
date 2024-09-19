f0040e93af03a4fee038da2098ca1ec7077eb0a2
Rubal <133045441+rahmed2210@users.noreply.github.com>
2024-09-19 09:08:48 -0400

    DOM

--- notes-app.js
+++ notes-app.js
@@ -1,29 +1,15 @@
-const notes = getSavedNotes()
+// DOM -- Document Object Model
 
-const filters = {
-    searchText: ''
-}
 
-renderNotes(notes, filters)
+// // Query and remove
+// const p = document.querySelector('p')
+// p.remove()
 
-document.querySelector('#create-note').addEventListener('click', function (e) {
-    notes.push({
-        id: uuidv4(),
-        title: '',
-        body: ''
-    }) 
-    saveNotes(notes)
-    renderNotes(notes, filters)
+// Query and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.textContent = ' ****** '
+//    console.log(p.textContent)
+//    p.remove()
 })
-
-// input event is going to fire on every single character changes
-//  --using input event with our text based inputs, allowing us to access that latest information
-document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
-})
-
