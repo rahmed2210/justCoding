4a7fe4caeff6e294fb959410a41d54bb20cfb4ee
Rubal <133045441+rahmed2210@users.noreply.github.com>
2024-09-19 09:07:20 -0400

    Add files via upload
    Upgrade Expense-tracker

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,52 +1,46 @@
-//expense-trackerPart2.js
-
-// --> Part 1
-// expenses --> description, amount
-// addExpense --> description, amount
-// getAccountSummary --> total up all expenses --> has $1250 in expenses
-
 const account = {
-  name: 'rubal ahmed',
-  expenses: [],
-  income: [], 
-  addExpense: function (description, amount) {
-    this.expenses.push({
-      description: description,
-      amount: amount
-    })
-  },
-  addIncome: function (description, amount) {
-    this.income.push({
-      description: description,
-      amount: amount
-    })
-  },
-  getAccountSummary: function () {
-    let totalExpenses = 0
-    let totalIncome = 0 
-    let accountBalance = 0 
-    
-    this.expenses.forEach(function (expense) {
-      totalExpenses = totalExpenses + expense.amount
-    })
-    
-    this.income.forEach(function (income) {
-      totalIncome = totalIncome + income.amount
-    })
-    
-    accountBalance = totalIncome - totalExpenses
-    
-    return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses`
-  }
+    name: 'Rubal',
+    expenses: [],
+    income: [],
+    addExpense: function (description, amount) {
+        this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    addIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
+    getAccountSummary: function () {
+        let totalExpenses = 0
+        let totalIncome = 0
+        let accountBalance = 0
+
+        this.expenses.forEach(function (expense) {
+            totalExpenses = totalExpenses + expense.amount
+        })
+
+        this.income.forEach(function (income) {
+            totalIncome = totalIncome + income.amount
+        })
+
+        accountBalance = totalIncome - totalExpenses
+
+        return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`
+    }
 }
 
-
-// --> Part 2
-// 1. add income array to account
-// 2. addIncome method -->description, amount
-// 3. tweak getAccountSummary
-
-account.addExpense('rent', 950)
-account.addExpense('coffee', 2)
-account.addIncome('job', 1000)
+account.addExpense('Rent', 950)
+account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
+
+// expense  --> description, amount
+// addExpense  --> description, amout
+// getAccountSummary  --> total up all expenses -- rubal has $1250 in expenses
+// add income array to account
+// addIncome method --> description, amount
+// tweak getaccountsummary
